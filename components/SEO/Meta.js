99af10ed2ed3f7db843e30221e05cee01070a559
Dhrumi Shah <dhrumishah358@gmail.com>
2023-07-16 00:58:09 +0530

    first commit

---
+++ components/SEO/Meta.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Dhrumi shah Portfolio </title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Dhrumi Shah Portfolio" />
+            <meta name="description"
+                content="Dhrumi Shah's (dhrumi5shah) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="Dhrumi Shah (dhrumi5shah)" />
+            <meta name="keywords"
+                content="dhrumi5shah, dhrumi5shah's portfolio, dhrumi5shah linux, ubuntu portfolio, dhrumi shah protfolio,dhrumi shah computer, dhrumi shah, dhrumi ubuntu, dhrumi shah ubuntu portfolio" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/fevicon.png" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="Dhrumi Shah Portfolio" />
+            <meta itemProp="description"
+                content="Dhrumi Shah's (dhrumi5shah) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/fevicon.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Dhrumi Shah Portfolio " />
+            <meta name="twitter:description"
+                content="Dhrumi Shah's (dhrumi5shah) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:site" content="dhrumi5shah" />
+            <meta name="twitter:creator" content="dhrumi5shah" />
+            <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta name="og:title" content="Dhrumi Shah Portfolio" />
+            <meta name="og:description"
+                content="Dhrumi Shah's (dhrumi5shah) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="og:image" content="images/logos/logo_1200.png" />
+            <meta name="og:url" content="https://github.com/dhrumi35" />
+            <meta name="og:site_name" content="Dhrumi Shah Personal Portfolio" />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+
+            <link rel="icon" href="images/logos/fevicon.svg" />
+            <link rel="apple-touch-icon" href="images/logos/logo.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
